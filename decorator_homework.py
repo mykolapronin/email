3e72d6c85ac898356f38f7c383e6d7556cde5697
Kolya <you@example.com>
2024-08-03 15:39:03 +0300

    ijfndsuin uirn8ugdsopja,u7hbnhy

--- decorator_homework.py
+++ decorator_homework.py
@@ -1,16 +1,13 @@
 def decorator_key_word_as_str(func):
     def operation_replacing(*args, **kwargs):
         operation = func(*args, **kwargs)
-        operation = str(operation)
-        print(*args)
-        print(kwargs)
-        return operation
+        return {"result": operation}
 
     return operation_replacing
 
 
 @decorator_key_word_as_str
-def final_word(text1: str, text2: str) -> str:
+def final_word(text1, text2) -> dict:
     return text1 + text2
 
 
